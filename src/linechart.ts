63b0bf7452bada8c6b4cd24885796c05930c259f
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2019-01-02 16:36:27 -0300

    Cuando el valor mínimo es mayor a cero se pone el mínimo igual a cero.

--- src/linechart.ts
+++ src/linechart.ts
@@ -9,7 +9,7 @@ export class LineChart extends SerialChart {
         c3Config: {
             axis: {
                 y: {
-                    padding: { bottom: 0, top: 25 }
+                    // padding: { bottom: 0, top: 25 }
                 }
             }
         }
@@ -21,8 +21,11 @@ export class LineChart extends SerialChart {
         let maxMin: any = Graphicator.calcularMinMax(this.getMatrix());
         let minYValue = (maxMin.min < 0 ? maxMin.min : (2 * maxMin.min - maxMin.max > 0 ? 2 * maxMin.min - maxMin.max : 0)); // acomoda el 0 automáticamente, si los datos útiles ocupan menos de la mitad cambio el 0        
 
-        this.config.c3Config.axis.y.min = minYValue; // truncating y axis
-        if (minYValue > 0) this.config.c3Config.grid.y.lines[0].value = minYValue; // draw horizontal line at botoom
+        if (minYValue > 0) {
+            minYValue = 0
+        }
+        this.config.c3Config.axis.y.min = minYValue;
+        
         this.config.c3Config.data.type = this.config.apilado ? 'area' : 'line';
     }
 }
